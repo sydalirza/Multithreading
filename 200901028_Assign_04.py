8ffb8a408a6035cbbfb56aa2e0aad0f73482dabf
Syed Ali Raza <92259618+sydalirza@users.noreply.github.com>
2023-01-03 21:34:34 +0500

    Update 200901028_Assign_04.py

--- 200901028_Assign_04.py
+++ 200901028_Assign_04.py
@@ -1,6 +1,6 @@
 import threading
 
-# Global variable to store the input string
+
 input_string = None
 
 def input_thread():
@@ -22,14 +22,14 @@ def shift_thread(input_string):
   print(shift_string)
 
 if __name__ == '__main__':
-  # Create the threads
+
   t1 = threading.Thread(target=input_thread, args=())
  
   # Start the input thread
   t1.start()
   t1.join()
 
-  # Start the other threads with the input string as an argument
+
   t2 = threading.Thread(target=reverse_thread, args=(input_string,))
   t3 = threading.Thread(target=capital_thread, args=(input_string,))
   t4 = threading.Thread(target=shift_thread, args=(input_string,))
